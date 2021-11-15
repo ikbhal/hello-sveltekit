35dbe89c4b2bd67367ac90abe37c4443c55be212
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2021-11-15 21:49:24 +0530

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<h1>Yaseen Daily Reminder</h1>
+<p>Daily read yasee, 70 hazaat small and big will fullfilled insha allah</p>
+<p>will be added link to update the status , done today or not</p>
+<p>link - to show all task history</p>
+<p>Chart actual, targetted</p>
+<p>link for google calendar</p>
+<p>Send benefit</p>
+<p>English translation</p>
+<p>Telugu Translation</p>
+<p>Cover missed target</p>
+<p>do extra reading when have time in weekends, when mood is good</p>
